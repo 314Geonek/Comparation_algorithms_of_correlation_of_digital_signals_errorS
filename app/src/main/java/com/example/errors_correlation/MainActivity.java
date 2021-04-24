3bf7323cfb1c2c9e6273839447342534d7d39df7
314Geonek <przychodzien.lukasz98@gmail.com>
2021-04-24 18:58:40 +0200

    +generateCode added

--- app/src/main/java/com/example/errors_correlation/MainActivity.java
+++ app/src/main/java/com/example/errors_correlation/MainActivity.java
@@ -3,13 +3,49 @@ package com.example.errors_correlation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner methodSelectorSpinner;
+    private EditText inputBitsEditText;
+    private TextView codedDataTextView;
+    private TextView decodedDataTextView;
+    private NumberPicker lengthOfGeneratedSeriesNumberPicker;
+    private TextView codedDataAfterCorrelationTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        methodSelectorSpinner = findViewById(R.id.methodSelectorSpinner);
+        inputBitsEditText = findViewById(R.id.inputBitsEditText);
+        codedDataTextView = findViewById(R.id.codedDataTextView);
+        codedDataAfterCorrelationTextView = findViewById(R.id.codedDataAfterCorrelationTextView);
+        lengthOfGeneratedSeriesNumberPicker = findViewById(R.id.lengthOfGeneratedSeriesNumberPicker);
+        lengthOfGeneratedSeriesNumberPicker.setValue(8);
+        String[] values = {"8","16","24"};
+        lengthOfGeneratedSeriesNumberPicker.setDisplayedValues(values);
+        //
+//        lengthOfGeneratedSeriesNumberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+//            @Override
+//            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+//                picker.setValue((newVal < oldVal)?oldVal-8:oldVal+8);
+//            }
+//        });
 
+  //      lengthOfGeneratedSeriesNumberPicker.setMinValue(8);
+    }
+
+    public void generateCode(View view) {
+        int codeLength = lengthOfGeneratedSeriesNumberPicker.getValue();
+        String generatedCode = "";
+        for(int i = 0; i < codeLength ; i++)
+        {
+            generatedCode = generatedCode.concat(Integer.toString((int)(Math.random() * (2))));
+        }
+        inputBitsEditText.setText(generatedCode);
     }
 }
